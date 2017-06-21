20360c79e2e9ca51d280e506c15ad8dd43f6d3fd
Yusuf Aji Wibowo <yusuf@skyshi.com>
2017-06-22 02:20:04 +0700

    Add header on recyclerview

--- app/src/main/java/net/crevion/flexcategory/CategoryAdapter.java
+++ app/src/main/java/net/crevion/flexcategory/CategoryAdapter.java
@@ -1,8 +1,8 @@
 package net.crevion.flexcategory;
 
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
@@ -12,7 +12,10 @@ import java.util.List;
  * Created by yusuf on 6/22/17.
  */
 
-public class CategoryAdapter extends RecyclerView.Adapter<CategoryViewHolder> {
+public class CategoryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_ITEM = 1;
 
     private List<String> stringList;
 
@@ -21,22 +24,46 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryViewHolder> {
     }
 
     @Override
-    public CategoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new CategoryViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_category, parent, false));
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_ITEM) {
+            return new CategoryViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_category, parent, false));
+        } else {
+            return new VHHeader(LayoutInflater.from(parent.getContext()).inflate(R.layout.header_category, parent, false));
+        }
     }
 
     @Override
-    public void onBindViewHolder(CategoryViewHolder holder, int position) {
-        holder.bindTo(stringList.get(position));
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof CategoryViewHolder) {
+            ((CategoryViewHolder) holder).bindTo(stringList.get(position));
+        }
     }
 
     @Override
     public int getItemCount() {
-        return stringList.size();
+        return stringList.size() + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isPositionHeader(position))
+            return TYPE_HEADER;
+
+        return TYPE_ITEM;
+    }
+
+    private boolean isPositionHeader(int position) {
+        return position == stringList.size();
     }
 
     public void addData(String string) {
         stringList.add(string);
         notifyDataSetChanged();
+    }
+
+    private class VHHeader extends RecyclerView.ViewHolder {
+        VHHeader(View itemView) {
+            super(itemView);
+        }
     }
 }
